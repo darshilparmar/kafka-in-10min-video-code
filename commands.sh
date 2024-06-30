fe3cdbb1a83a77e2157f745485234bf962a3b524
Darshil Parmar <22773771+darshilparmar@users.noreply.github.com>
2024-06-30 13:05:47 +0530

    Add files via upload

---
+++ commands.sh
@@ -0,0 +1,24 @@
+
+#Access Kafka container:
+docker exec -it <kafka_container_id> /bin/bash
+
+# Locate Kafka binaries:
+cd /opt/kafka/bin
+
+# create topic 
+./kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1
+
+# Run Kafka Producer
+./kafka-console-producer.sh --topic test-topic --bootstrap-server localhost:9092
+
+# Run Kafka Consumer:
+docker exec -it <kafka_container_id> /bin/bash
+cd /opt/kafka/bin
+./kafka-console-consumer.sh --topic test-topic --bootstrap-server localhost:9092 --from-beginning
+
+
+## Creating a Topic with Multiple Partitions:
+docker exec -it <kafka_container_id> /bin/bash
+cd /kafka/bin
+./kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3
+
