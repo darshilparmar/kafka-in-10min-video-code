8c060d467dba6a26fb6c40232ea9c1aabaf4abec
Shubham Rawat <45076771+shubhamsingrawat@users.noreply.github.com>
2024-08-06 13:41:12 +0530

    Update commands.sh
    fixed the path issue /opt is missing while creating 2nd topic
    
    updated the topic name , it will throw error for same topic name is used

--- commands.sh
+++ commands.sh
@@ -17,8 +17,8 @@ cd /opt/kafka/bin
 ./kafka-console-consumer.sh --topic test-topic --bootstrap-server localhost:9092 --from-beginning
 
 
-## Creating a Topic with Multiple Partitions:
+## Creating a Topic with Multiple Partitions: 
 docker exec -it <kafka_container_id> /bin/bash
-cd /kafka/bin
-./kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3
+cd /opt/kafka/bin
+./kafka-topics.sh --create --topic test-topic-two --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3 # it will throw error if topic is already there
 
